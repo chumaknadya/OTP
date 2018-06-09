0b1e752b4ef5a1529670ba01da2013a57192feea
chumalnadya19 <chumalnadya19@gmail.com>
2018-06-09 16:20:09 +0300

    fix

--- OOP_lab2/src/pack1/Abst_System.java
+++
@@ -1,5 +0,0 @@
-package pack1;
-
-public abstract class Abst_System {
-    public abstract  void AddToSystem(User u);
-}
--- OOP_lab2/src/pack2/Main.java
+++
@@ -1,8 +0,0 @@
-package pack2;
-
-public class Main {
-    public static void main(String[] args) {
-        GameControl game = new GameControl("Tanchiki");
-        game.Mouse_Attack();
-    }
-}
